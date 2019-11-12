329e43e3b9346cd1d8c5cd6a3dddcd17e6e1c7c8
Evaldas Paulauskas <s033972@ad.viko.lt>
2019-11-12 22:39:31 +0200

    init

---
+++ StringToCharAndSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//input 127.0.0.1
+/*
+
+add all numbers together
+multiply by first digit
+output in console
+
+*/
+
+
+namespace StringToCharAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
