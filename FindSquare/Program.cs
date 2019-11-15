9f6cc8edd53af0893a9fe84c03ab5e057e0e1e4e
crowjambo <evaldascrow@gmail.com>
2019-11-15 23:04:37 +0200

    solved

---
+++ FindSquare/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FindSquare
+{
+    class Program
+    {
+        /*
+
+        isSquare(-1) returns  false
+        isSquare(0) returns   true
+        isSquare(3) returns   false
+        isSquare(4) returns   true
+        isSquare(25) returns  true  
+        isSquare(26) returns  false
+
+        */
+
+        public static bool isSquare(int n){
+            int i = 0;
+            while(i*i <= n){
+                if(i * i == n){
+                    return true;
+                }
+                i++;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int input = 4;
+            Console.WriteLine(isSquare(input).ToString());
+        }
+    }
+}
