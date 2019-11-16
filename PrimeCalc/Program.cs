29bc58bdc42562be767acfc830781440c0da9876
Evaldas <evaldascrow@gmail.com>
2019-11-16 12:06:35 +0200

    primes solution

---
+++ PrimeCalc/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PrimeCalc
+{
+    class Program
+    {
+        /*
+        
+        Define a function that takes an integer argument and returns logical value true or false depending on if the integer is a prime.
+        
+        */
+
+        public static bool IsPrime(int n)
+        {
+            if(n <= 1) return false;
+            int count = 0;
+            for(int i = 1; i<10; i++){
+                if(n % i == 0){
+                    count++;
+                } 
+            }
+            if(n > 9){
+                count++;
+            }
+            if(count == 2) return true;
+            return false;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPrime(73).ToString());
+        } 
+    }
+}
