863e1a9c22748c4e9bdc60b8330e93c46ecaba5e
Evaldas <evaldascrow@gmail.com>
2019-11-21 17:47:18 +0200

    solved

--- GreedIsGood/Program.cs
+++ GreedIsGood/Program.cs
@@ -23,71 +23,58 @@ A single die can only be counted once in each roll. For example, a "5" can only 
     Assert.AreEqual(0, Kata.Score(new int[] {2, 3, 4, 6, 2}), "Should be 0 :-(");
 
     
+ Throw       Score
+ ---------   ------------------
+ 5 1 3 4 1   50 + 2 * 100 = 250
+ 1 1 1 3 1   1000 + 100 = 1100
+ 2 4 4 5 4   400 + 50 = 450
+
     */
     class Program
     {
         public static int Score(int[] dice) {
+            //vars
             int points = 0;
+            List<int> temp;
             Dictionary<int,List<int>> diceCollection = new Dictionary<int, List<int>>();
+            //init dictionary key values
             for (int i = 0; i < 6; i++)
             {
                 diceCollection.Add(i+1,new List<int>());
             }
-            List<int> temp;
+            //collect dices in list collections
             foreach(var x in dice){
                 diceCollection.TryGetValue(x, out temp);
                 temp.Add(x);
             }
+
+            //points calc
             var lists = diceCollection.Values;
             int counter = 1;
             foreach(var list in lists){
-                switch(counter){
-                    case 1:
-                        if(list.Count == 1){
-                            points += 100;
-                        }
-                        else if(list.Count >= 3){
-                            points += 1000;
-                        }
-                        break;
-                    case 2:
-                        if(list.Count >= 3){
-                            points += 200;
-                        }
-                        break;
-                    case 3:
-                        if(list.Count >= 3){
-                            points += 300;
-                        }
-                        break;
-                    case 4:
-                        if(list.Count >= 3){
-                            points += 400;
-                        }
-                        break;
-                    case 5:
-                        if(list.Count == 1){
-                            points += 50;
-                        }
-                        else if(list.Count >= 3){
-                            points += 500;
-                        }
-                        break;
-                    case 6:
-                        if(list.Count >= 3){
-                            points += 600;
-                        }
-                        break;                    
+                if(list.Count ==3){
+                    if(counter == 1) points += 1000;
+                    else points += counter*100;
+                }
+                else if(list.Count > 3){
+                    if(counter == 1) points += 1100;
+                    else if(counter == 5) points += 550; 
+                    else points += counter*100;                
+                }
+                else if(list.Count < 3){
+                    if(counter == 1) points += 100 * list.Count;
+                    else if(counter == 5) points += 50 *list.Count;
                 }
                 counter++;
             }
-
+            //out
             return points;
         }
 
+
         static void Main(string[] args)
         {
-            int[] input = new int[]{4,4,4,3,3};
+            int[] input = new int[]{5,2,5,2,2};
             Console.WriteLine(Score(input));
         }
     }
