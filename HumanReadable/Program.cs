668735493a7f1b7280504228a7ccb57e8d333518
Evaldas <evaldascrow@gmail.com>
2019-11-30 18:24:49 +0200

    worst code ive ever written

--- HumanReadable/Program.cs
+++ HumanReadable/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HumanReadable
 {
@@ -35,14 +37,79 @@ A unit of time must be used "as much as possible". It means that the function sh
          */
 
         public static string formatDuration(int seconds){
-            //Enter Code here
+            //special case for 0
+            if(seconds == 0) return "now";
 
-            return "";
+            int[] sizes = new int[]{3600*24*365, 3600*24, 3600, 60};
+            string[] timeStrings = new string[]{"year", "day", "hour", "minute", "second"};
+            string output = String.Empty;
+            int counter;
+            //general collection for time marks
+            List<int> col = new List<int>();
+
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                //count occurances
+                counter = 0;
+                //decrement starting from biggest
+                while(seconds >= sizes[i]){
+                    seconds -= sizes[i];
+                    counter++;
+                }
+                //add all occurances for each size
+                col.Add(counter);
+            }
+            //add whats left, the seconds
+            col.Add(seconds);
+
+            //create string based on the data
+            for (int i = 0; i < col.Count; i++)
+            {
+                //time mark exists
+                if(col[i] != 0){
+                    output += $"{col[i].ToString()} {timeStrings[i]}";
+                    //plural
+                    if(col[i] > 1) output += "s";
+
+
+                    //check whether there is another timemark
+                    if(col[i] != col.Last()){
+
+                        try{
+                            //decide to add , or and
+                            //if next is not last
+                            if(col[i+2] != 0){
+                                output += ", ";
+                            }
+                            else{
+                                //next element is last
+                                output += " and ";
+                                
+                            }
+                        }
+                        catch(Exception){
+                            if(col[i+1] != 0)
+                            output += " and ";
+                        }
+
+                    }
+
+                }
+            }
+
+            return output;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+//             formatDuration (62)    // returns "1 minute and 2 seconds"
+//             formatDuration (3662)  // returns "1 hour, 1 minute and 2 seconds"
+            // Assert.AreEqual("7 years, 246 days, 15 hours, 32 minutes and 54 seconds",HumanTimeFormat.formatDuration(242062374));
+    // Assert.AreEqual("4 years, 68 days, 3 hours and 4 minutes",HumanTimeFormat.formatDuration(132030240));
+
+
+            int input = 120;
+            System.Console.WriteLine(formatDuration(input));
         }
     }
 }
