948f30f8f972684ef9a3878bb7b2baf3c6a88030
Evaldas <evaldascrow@gmail.com>
2019-12-01 05:48:49 +0200

    factorial practice

---
+++ FactorialNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FactorialNumbers
+{
+    /* n! = n * (n-1) * (n-2) 
+    
+        5! = 5 * 4 *3 *2 *1 = 120
+    */
+    class Program
+    {
+        public static int FactorialNumbers(int n){
+            if(n == 1) return n;
+            //recursive funcs need to be always decrementing, or not even be called, else its infinite loop!
+            return n * FactorialNumbers(n-1);
+        }
+
+        static void Main(string[] args)
+        {
+            int input = 5;
+            Console.WriteLine(FactorialNumbers(input).ToString());
+        }
+    }
+}
