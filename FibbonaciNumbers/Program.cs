f4bef8260b934a4507187f79db74b7f6c0af5072
Evaldas <evaldascrow@gmail.com>
2019-12-01 05:39:05 +0200

    fib numbers

---
+++ FibbonaciNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FibbonaciNumbers
+{
+    /* 
+    
+        F0 = 0; F1 = 1;
+        Fn = fn-1 + fn-2
+
+        0,1,1,2,3,,5,8,13
+
+        input = n
+
+        fib(9) = 34
+
+    */
+
+    class Program
+    {
+        public static int FibbonaciNumbers(int n){
+            if(n <= 1) return n;
+            return FibbonaciNumbers(n-1) + FibbonaciNumbers(n-2);
+        }
+
+        static void Main(string[] args)
+        {
+            int input = 9;
+            System.Console.WriteLine(FibbonaciNumbers(input).ToString());
+        }
+    }
+}
